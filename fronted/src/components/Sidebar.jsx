b5ed0f2eb052917ce44c4a7c3448ebb0e020511e
Subrata-003 <subratamandal3328@gmail.com>
2025-08-27 20:13:31 +0530

    sidebar modified

--- fronted/src/components/Sidebar.jsx
+++ fronted/src/components/Sidebar.jsx
@@ -29,8 +29,8 @@ const Sidebar = () => {
                     <span>Home</span>
                 </Link>
                 <Link
-                    to="/friends"
-                    className={`btn btn-ghost justify-start w-full gap-3 px-3 normal-case ${currentPath === "/friends" ? "btn-active" : ""
+                    to="/"
+                    className={`btn btn-ghost justify-start w-full gap-3 px-3 normal-case ${currentPath === "/" ? "btn-active" : ""
                         }`}
                 >
                     <UsersIcon className="size-5 text-base-content opacity-70" />
